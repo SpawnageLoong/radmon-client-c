5e166e160b29246260566a7fcbbb03fe83ad8d7f
Richard Loong <richardloongcj@gmail.com>
2025-02-03 08:54:48 +0000

    Add full and partial fram dump

--- src/main.c
+++ src/main.c
@@ -28,16 +28,25 @@
 
 using namespace std;
 
-void print_menu();
+#define CMD_FRAME_ID 0x010;
+#define RSP_FRAME_ID 0x011;
+
+void print_logo();
+void main_menu(int* user_input);
+void command_can_dump_full();
+void command_can_dump_part();
+
+int s;
+struct can_frame frame;
 
 int main()
 {
-    int can_active;
+    int user_input;
+    int is_exit = 0;
+
     int ret;
-    int s, nbytes;
     struct sockaddr_can addr;
     struct ifreq ifr;
-    struct can_frame frame;
     
     memset(&frame, 0, sizeof(struct can_frame));
     system("clear");
@@ -46,15 +55,13 @@ int main()
     // Check if can0 interface is active, activate it if not
     FILE *fp = popen("ip link show | grep \"can0\"", "r");
     if (fp == NULL) {
-        cout << "can0 interface not active, enabling now...\n";
-        can_active = 0;
+        cout << "can0 interface not detected, enabling now...\n";
         system("sudo ip link set can0 type can bitrate 100000");
-        system("sudo ifconfig can0 up");
         cout << "can0 interface enabled\n\n";
     } else {
-        cout << "can0 interface already active\n\n";
-        can_active = 1;
+        cout << "can0 interface detected\n\n";
     }
+    system("sudo ifconfig can0 up");
     
     //printf("this is a can receive demo\r\n");
     
@@ -87,10 +94,35 @@ int main()
     rfilter[0].can_id = 0x123;
     rfilter[0].can_mask = CAN_SFF_MASK;
     setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, &rfilter, sizeof(rfilter));
+    
 
-    print_menu();
+    //4.Disable filtering rules, do not receive packets, only send
+    //setsockopt(s, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+
+    print_logo();
+    while (!is_exit) {
+        main_menu(&user_input);
+        switch (user_input) {
+            case 1:
+                command_can_dump_full();
+                break;
+            case 2:
+                command_can_dump_part();
+                break;
+            case 9:
+                is_exit = 1;
+                break;
+            default:
+                cout << "\nUnknown command received\nEnter a number 1-9: ";
+                break;
+        }
+    }
+    
+
+    
 
     //5.Receive data and exit
+    /*
     while(1) {
         nbytes = read(s, &frame, sizeof(frame));
         if(nbytes > 0) {
@@ -101,24 +133,146 @@ int main()
             break;
         }
     }
+    */
     
-    //6.Close the socket and can0
+    // Close the socket and can0
+    cout << "Exiting...\n\n";
     close(s);
-    if (can_active == 1) {
-        cout << "Exiting...";
-        system("sudo ifconfig can0 down");
-    }
-    
+    system("sudo ifconfig can0 down");
     return 0;
 }
 
-void print_menu() {
+void print_logo() {
     cout << "\n██████   █████  ██████  ███    ███  ██████  ███    ██      ██████  █████  ███    ██ ";
     cout << "\n██   ██ ██   ██ ██   ██ ████  ████ ██    ██ ████   ██     ██      ██   ██ ████   ██ ";
     cout << "\n██████  ███████ ██   ██ ██ ████ ██ ██    ██ ██ ██  ██     ██      ███████ ██ ██  ██ ";
     cout << "\n██   ██ ██   ██ ██   ██ ██  ██  ██ ██    ██ ██  ██ ██     ██      ██   ██ ██  ██ ██ ";
     cout << "\n██   ██ ██   ██ ██████  ██      ██  ██████  ██   ████      ██████ ██   ██ ██   ████ ";
     cout << "\n";
+    return;
+}
+
+void main_menu(int* user_input) {
     cout << "\n____________________________________________________________________________________";
     cout << "\n";
+    cout << "\nAvailable Options:";
+    cout << "\n  1) Dump FRAM (32kB)";
+    cout << "\n  2) Dump FRAM (Select a sector)";
+    cout << "\n";
+    cout << "\n  9) Exit";
+    cout << "\n";
+    cout << "\nEnter a number 1-9: ";
+
+    scanf("%d", user_input);
+    cout << "\n";
+
+    return;
+}
+
+void command_can_dump_full() {
+    // Set send data
+    frame.can_id = CMD_FRAME_ID;
+    frame.can_dlc = 8; // data length
+    frame.data[0] = 0x01;
+    frame.data[1] = 0x00;
+    frame.data[2] = 0x00;
+    frame.data[3] = 0x00;
+    frame.data[4] = 0x00;
+    frame.data[5] = 0x00;
+    frame.data[6] = 0x00;
+    frame.data[7] = 0x00;
+    
+    int nbytes;
+    printf("can_id  = 0x%X\r\n", frame.can_id);
+    printf("can_dlc = %d\r\n", frame.can_dlc);
+    for(int i = 0; i < 8; i++)
+        printf("data[%d] = %d\r\n", i, frame.data[i]);
+    
+    // Send message
+    nbytes = write(s, &frame, sizeof(frame)); 
+    cout << "\nmessage sent\n";
+    if(nbytes != sizeof(frame)) {
+        printf("Send Error frame[0]!\r\n");
+    }
+    return;
+}
+
+void command_can_dump_part() {
+    int is_input_valid;
+    char user_hex_input[4];
+    char address_H[2];
+    char address_L[2];
+    char length_H[2];
+    char length_L[2];
+
+    // Set send data
+    frame.can_id = CMD_FRAME_ID;
+    frame.can_dlc = 8; // data length
+    frame.data[0] = 0x02;
+    frame.data[5] = 0x00;
+    frame.data[6] = 0x00;
+    frame.data[7] = 0x00;
+
+    // Get param A (address)
+    is_input_valid = 0;
+    while (is_input_valid == 0) {
+        is_input_valid = 1;
+        cout << "\nEnter the starting address (4 hex-digits): ";
+        
+        scanf("%s", user_hex_input);
+        cout << "\n";
+
+        for (int i=0; i<4; i++) {
+            if ( !user_hex_input[i] || 'f' < user_hex_input[i] || user_hex_input[i] < '0') {
+                cout << "\nInvalid input. Please enter a 4 digit hex value (0 - F).\n";
+                is_input_valid = 0;
+                break;
+            }
+        }
+        address_H[0] = user_hex_input[0];
+        address_H[1] = user_hex_input[1];
+        address_L[0] = user_hex_input[2];
+        address_L[1] = user_hex_input[3];
+    }
+
+    // Get param B (length)
+    is_input_valid = 0;
+    while (is_input_valid == 0) {
+        is_input_valid = 1;
+        cout << "\nEnter the length to read (4 hex-digits): ";
+        
+        scanf("%s", user_hex_input);
+        cout << "\n";
+
+        for (int i=0; i<4; i++) {
+            if ( !user_hex_input[i] || 'f' < user_hex_input[i] || user_hex_input[i] < '0') {
+                cout << "\nInvalid input. Please enter a 4 digit hex value (0 - F).\n";
+                is_input_valid = 0;
+                break;
+            }
+        }
+        length_H[0] = user_hex_input[0];
+        length_H[1] = user_hex_input[1];
+        length_L[0] = user_hex_input[2];
+        length_L[1] = user_hex_input[3];
+    }
+    
+    frame.data[1] = (unsigned char)strtol(address_H, NULL, 16);
+    frame.data[2] = (unsigned char)strtol(address_L, NULL, 16);
+    frame.data[3] = (unsigned char)strtol(length_H, NULL, 16);
+    frame.data[4] = (unsigned char)strtol(length_L, NULL, 16);
+    
+    int nbytes;
+    printf("can_id  = 0x%X\r\n", frame.can_id);
+    printf("can_dlc = %d\r\n", frame.can_dlc);
+    for(int i = 0; i < 8; i++)
+        printf("data[%d] = %d\r\n", i, frame.data[i]);
+    
+    // Send message
+    nbytes = write(s, &frame, sizeof(frame)); 
+    cout << "\nmessage sent\n";
+    if(nbytes != sizeof(frame)) {
+        printf("Send Error frame[0]!\r\n");
+    }
+    return;
 }
